d0ff2b21e07f8d4c81633117615905c485818039
xxDOOMbox <shawwian@gmail.com>
2018-06-03 21:45:43 -0400

    ch 4 exercise

---
+++ functions/ex4_1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int strrindex(char s[], char t[]);
+
+int main() 
+{
+    char str1[] = "Tabithabitha";
+    char str2[] = "bit";
+
+    int index = strrindex(str1, str2);
+
+    if (index == -1) 
+        printf("The word %s does not appear in the word %s.\n", str2, str1); 
+    else
+        printf("The word %s appears in the word %s at rightmost index %i.\n", str2, str1, index);
+
+    return 0; 
+}
+
+int strrindex(char s[], char t[])
+{
+    int c1 = 0;
+    int c2 = 0;
+    int index = -1;
+    int i = 0; 
+
+    for (i=0; s[i] != '\0'; i++)
+    {
+        if (s[i] == t[0])
+        {
+            c1 = i;
+            c2 = 0; 
+            while (s[c1++] == t[c2++])
+                 ;
+            if (t[c2] == '\0')
+            {
+                index = i;
+            }
+        }
+    }
+
+    return index; 
+}
